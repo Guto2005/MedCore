64f491add110477c1026381f04a283fea88fef02
unknown <gustavo@gpsbr.net>
2026-05-31 17:49:34 -0300

    feat: criação do modelo de médicos

--- medicos/models.py
+++ medicos/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from usuarios.models import Usuario
 
-# Create your models here.
+
+class Medico(models.Model):
+    usuario = models.OneToOneField(
+        Usuario,
+        on_delete=models.CASCADE
+    )
+
+    crm = models.CharField(
+        max_length=20,
+        unique=True
+    )
+
+    especialidade = models.CharField(
+        max_length=100
+    )
+
+    telefone = models.CharField(
+        max_length=20
+    )
+
+    def __str__(self):
+        return f"{self.usuario.nome} - {self.especialidade}"
