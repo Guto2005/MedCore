d4074235c06dfa33422a6b8fa1fdac7123526d9e
unknown <gustavo@gpsbr.net>
2026-05-31 17:56:22 -0300

    feat: criação do modelo de pacientes

--- pacientes/models.py
+++ pacientes/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+
+class Paciente(models.Model):
+    SEXOS = [
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+        ('O', 'Outro'),
+    ]
+
+    nome = models.CharField(max_length=100)
+
+    cpf = models.CharField(
+        max_length=14,
+        unique=True
+    )
+
+    data_nascimento = models.DateField()
+
+    sexo = models.CharField(
+        max_length=1,
+        choices=SEXOS
+    )
+
+    telefone = models.CharField(max_length=20)
+
+    telefone_emergencia = models.CharField(
+        max_length=20,
+        blank=True
+    )
+
+    email = models.EmailField(
+        blank=True
+    )
+
+    convenio = models.CharField(
+        max_length=100,
+        blank=True
+    )
+
+    alergias = models.TextField(
+        blank=True
+    )
+
+    doencas_geneticas = models.TextField(
+        blank=True
+    )
+
+    observacoes = models.TextField(
+        blank=True
+    )
+
+    data_cadastro = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.nome
