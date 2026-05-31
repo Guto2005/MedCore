5591884ec560ac92cfe9cdd37803f175de847d5c
unknown <gustavo@gpsbr.net>
2026-05-31 17:38:42 -0300

    feat: criação do modelo de usuários

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Usuario(models.Model):
+    TIPOS_USUARIO = [
+        ('ADMIN', 'Administrador'),
+        ('MEDICO', 'Médico'),
+        ('USUARIO', 'Usuário'),
+    ]
+
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    tipo_usuario = models.CharField(
+        max_length=10,
+        choices=TIPOS_USUARIO,
+        default='USUARIO'
+    )
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
