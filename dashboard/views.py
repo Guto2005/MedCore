a868734dd1efe5333b62959ae82944137c7e1d3f
unknown <gustavo@gpsbr.net>
2026-06-05 19:22:52 -0300

    feat: integrar dashboard com dados do banco

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from pacientes.models import Paciente
+from medicos.models import Medico
+from atendimentos.models import Atendimento
+
+
 @login_required(login_url='login')
 def home(request):
+
+    context = {
+
+        'total_pacientes': Paciente.objects.count(),
+
+        'total_medicos': Medico.objects.count(),
+
+        'total_atendimentos': Atendimento.objects.count(),
+
+    }
+
     return render(
         request,
-        'dashboard/home.html'
+        'dashboard/home.html',
+        context
     )
